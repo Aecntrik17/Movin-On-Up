d808f590d431ef418ab4a93c906248e90d66e5f6
Dale Foskey <dalefoskey@icloud.com>
2020-06-30 18:37:05 -0400

    updates

--- Assets/script.js
+++ Assets/script.js
@@ -5,30 +5,32 @@ var apikeygoogle = 'AIzaSyCUhLVjRWl_hRcOuGutBWR_QwLWLQJWaSA';
 var apikeymapquest = 'wkAXQtPfXHFVQVVsUyUHn1VONKEaiGuR';
 //should be directed to user input once merged
 
-function submitFunction(){
+function submitFunction() {
 	event.preventDefault();
-	let city = document.getElementById("city-input").value;
+	let city = document.getElementById('city-input').value;
 
-	//ajax request from mapquest for list of apartments in the search area  *** not finished ***
-	// $.ajax({
-	// 	url:
-	// 		'https://www.mapquestapi.com/search/v2/radius?origin=' +
-	// 		city +
-	// 		'&radius=15&maxMatches=3&ambiguities=ignore&hostedData=mqap.ntpois|group_sic_code=?|651303&outFormat=json&key=' +
-	// 		apikeymapquest,
-	// 	method: 'GET'
-	// }).then(function(response) {
-	// 	console.log(response);
-	// 	let apartment1 = response.searchResults[0].name;
-	// 	let address1 = response.searchResults[0].fields.address;
-	// 	let city1 = response.searchResults[0].fields.city;
-	// 	let apartment2 = response.searchResults[1].name;
-	// 	let address2 = response.searchResults[1].fields.address;
-	// 	let city2 = response.searchResults[1].fields.city;
-	// 	let apartment3 = response.searchResults[2].name;
-	// 	let address3 = response.searchResults[2].fields.address;
-	// 	let city3 = response.searchResults[2].fields.city;
-	// });
+	// ajax request from mapquest for list of apartments in the search area  *** not finished ***
+	$.ajax({
+		url:
+			'https://www.mapquestapi.com/search/v2/radius?origin=' +
+			city +
+			'&radius=15&maxMatches=3&ambiguities=ignore&hostedData=mqap.ntpois|group_sic_code=?|651303&outFormat=json&key=' +
+			apikeymapquest,
+		method: 'GET'
+	}).then(function(response) {
+		console.log(response);
+		let htmlbox2 = document.getElementById('content');
+		let htmlBox2Content = '';
+
+		for (let i = 0; i < 3; i++) {
+			let apartment = response.searchResults[i].name;
+			let address = response.searchResults[i].fields.address;
+			let city = response.searchResults[i].fields.city;
+			htmlBox2Content +=
+				'<p>Name: ' + apartment + '</p><br><p>Address: ' + address + ' City: ' + city + '</p><br>';
+		}
+		htmlbox2.innerHTML = htmlBox2Content;
+	});
 
 	//ajax request from mapquest for list of schools in the search area
 	$.ajax({
@@ -40,28 +42,25 @@ function submitFunction(){
 		method: 'GET'
 	}).then(function(response) {
 		// list top 3 schools
-		let htmlbox = document.getElementById('content');
-		let htmlBoxContent = "";
+		let htmlbox = document.getElementById('content1');
+		let htmlBoxContent = '';
 
 		for (let i = 0; i < 3; i++) {
 			let school = response.searchResults[i].name;
 			let schooladdress = response.searchResults[i].fields.address;
 			let schoolcity = response.searchResults[i].fields.city;
-			htmlBoxContent += ("<p>Name: " + school + "</p><br><p>Address: " + schooladdress + " City: " + schoolcity + "</p><br>");
+			htmlBoxContent +=
+				'<p>Name: ' + school + '</p><br><p>Address: ' + schooladdress + ' City: ' + schoolcity + '</p><br>';
 		}
 		htmlbox.innerHTML = htmlBoxContent;
 
 		// update google map based on the city geo locations from the api call.
 		map = new google.maps.Map(document.getElementById('map'), {
-			center: {lat: response.origin.latLng.lat, lng: response.origin.latLng.lng},
+			center: { lat: response.origin.latLng.lat, lng: response.origin.latLng.lng },
 			zoom: 10
 		});
-
 	});
-
 }
-
-
 
 // initial geo location for map
 
